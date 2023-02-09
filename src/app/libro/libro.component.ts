caa6d8d774ed2c8b6ac095a98517f5ca50fb803c
Maxito06 <maxicopi5@gmail.com>
2023-02-08 19:26:19 -0500

    mejorado

--- src/app/libro/libro.component.ts
+++ src/app/libro/libro.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-libro',
   templateUrl: './libro.component.html',
   styleUrls: ['./libro.component.css']
 })
-export class LibroComponent {
+export class LibroComponent implements OnInit {
+
+  titulo = "Cien años de soledad";
+
+  constructor(){
+
+
+  }
+
+  ngOnInit(): void{
+    
+  }
 
 }
+

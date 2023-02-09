de57eb151a4d00d68e3c8ca4ad69da49b6a3d4e7
Maxito06 <maxicopi5@gmail.com>
2023-02-08 19:37:20 -0500

    mejorado2

--- src/app/libro/libro.component.ts
+++ src/app/libro/libro.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Libro } from '../modelos/libro';
+import { LibroService } from '../servicios/libro.service';
 
 @Component({
   selector: 'app-libro',
@@ -7,16 +10,84 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LibroComponent implements OnInit {
 
-  titulo = "Cien años de soledad";
+  libros:Array<Libro> = new Array<Libro>();
+  modal = false;
 
-  constructor(){
+  // @ts-ignore
+  libroForm: FormGroup;
+  libro_up:Libro;
 
+  constructor(private servicio: LibroService,private fbGenerator: FormBuilder) { 
+    this.libro_up = new Libro();
+       
+  }
+
+  ngOnInit(): void {
+    this.libroForm = this.fbGenerator.group({
+      autor:[3],
+      categoria:[1],
+      titulo:[''],
+      descripcion:[''],
+      fecha_pub:['']
+    })
+
+    this.servicio.getLibros().subscribe((libros)=>{
+      this.libros = libros;
+    });
+  }
+
+  new_libro(){
+    this.libroForm.setValue({
+      autor:3,
+      categoria:1,
+      titulo:'',
+      descripcion:'',
+      fecha_pub:''
+    });
+  }
+
+  add_libro(){
+    var libro = this.libroForm.value as Libro;
+    console.log(libro)
+    this.servicio.saveLibro(libro).subscribe((libroNuevo)=>{
+      this.servicio.getLibros().subscribe((libros)=>{
+        this.libros = libros;
+        this.modal = false;
+      });
+    });
+  }
+
+  update_libro(){
+    var libro = this.libroForm.value as Libro;
+    libro.id = this.libro_up.id
+    this.servicio.updateLibro(libro).subscribe((libroNuevo)=>{
+      this.servicio.getLibros().subscribe((libros)=>{
+        this.libros = libros;
+        this.modal = false;
+      });
+    });
 
   }
 
-  ngOnInit(): void{
-    
+  see_update(libro:Libro){
+    this.libro_up = libro;
+    this.libroForm.setValue({
+      autor:libro.autor,
+      categoria:libro.categoria,
+      titulo:libro.titulo,
+      descripcion:libro.descripcion,
+      fecha_pub:libro.fecha_pub
+    });
+  }
+
+
+  delete_libro(libro:Libro){
+    this.servicio.deleteLibro(libro).subscribe((response)=>{
+      this.servicio.getLibros().subscribe((libros)=>{
+        this.libros = libros;
+        this.modal = false;
+      });
+    });
   }
 
 }
-
